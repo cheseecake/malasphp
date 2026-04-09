cfd21f5e23297bc6ab254e8a96dc3ae2737b641a
nadchan <nadiaachan@icloud.com>
2026-04-09 13:33:10 +0700

    Update database name in koneksi.php

--- nadchan/koneksi.php
+++ nadchan/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "nadchan");
+$koneksi = mysqli_connect("localhost", "root", "", "nadchanSECRET");
 
 if (mysqli_connect_errno()){
     echo "Koneksi database gagal : " . mysqli_connect_error();
